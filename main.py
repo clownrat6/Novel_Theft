56bab8ef0ab7bdbb79655066e8af0dae1d2224c4
sennnnn <58427300+sennnnn@users.noreply.github.com>
2020-05-03 12:44:35 +0800

    修复了找不到 arg_parser 的 bug 修复 main_process 找不到的 bug

--- main.py
+++ main.py
@@ -5,27 +5,10 @@ import _thread
 
 from epub import epub_zip
 from util import parse_info
+from arg_parser import arg_parser
 from parse import one_stage_parse,two_stage_parse
 from thread import download_txt,download_illustration
 from concurrent.futures import wait,ALL_COMPLETED
-
-a = arg_parser()
-
-a.add_val('--url', 'none')
-a.add_val('--txt', 'none')
-
-parse_dict = a()
-
-if(parse_dict['--url'] != 'none'):
-    # 现在是以单本书为单元的程序了
-    # 主页 url
-    main_page_url = parse_dict['--url']
-    main_process(main_page_url)
-elif(parse_dict['--txt'] != 'none'):
-    txt_path = parse_dict['--txt']
-    url_list = parse_info(txt_path)
-    for url in url_list:
-        main_process(url)
 
 def main_process(_url):
     # 获取小说文字，插图，名字，作者
@@ -41,3 +24,21 @@ def main_process(_url):
     wait(all_task, return_when=ALL_COMPLETED)
     # 打包成 eupb
     epub_zip(novel_name, author_name, 'test', build_dir)
+
+if __name__ == '__main__':
+    a = arg_parser()
+
+    a.add_val('--url', 'none')
+    a.add_val('--txt', 'none')
+
+    parse_dict = a()
+    if(parse_dict['--url'] != 'none'):
+        # 现在是以单本书为单元的程序了
+        # 主页 url
+        main_page_url = parse_dict['--url']
+        main_process(main_page_url)
+    elif(parse_dict['--txt'] != 'none'):
+        txt_path = parse_dict['--txt']
+        url_list = parse_info(txt_path)
+        for url in url_list:
+            main_process(url)
